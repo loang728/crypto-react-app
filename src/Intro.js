0a26a0817b3c86b9b6c4b5636857e69a1d59d476
lora <loang728@gmail.com>
2024-03-20 14:49:53 +0200

    updates

--- src/Intro.js
+++ src/Intro.js
@@ -1,4 +1,4 @@
-import './Intro.css';
+import "./Intro.css";
 
 function Intro() {
   return (
@@ -6,19 +6,21 @@ function Intro() {
       <div className="intro-aligner">
         <h1>
           <span>trade with</span>
-          <strong>Zero Swaps</strong> on All CRYPTO Ultra Low Accounts for more than 25
-          instruments!
+          <strong>Zero Swaps</strong> on All CRYPTO Ultra Low Accounts for more
+          than 25 instruments!
         </h1>
         <p>
-          Enjoy spreads as low as 0.6 pips and leverage up to 1000:1 on instruments
-          like <strong>EURUSD</strong>, <strong>USDJPY</strong>,{" "}
-          <strong>EURJPY</strong>, <strong>GBPUSD</strong>, and <strong>Gold</strong>.
+          Enjoy spreads as low as 0.6 pips and leverage up to 1000:1 on
+          instruments like <strong>EURUSD</strong>, <strong>USDJPY</strong>,{" "}
+          <strong>EURJPY</strong>, <strong>GBPUSD</strong>, and{" "}
+          <strong>Gold</strong>.
         </p>
         <a className="cta open-account"> open account </a>
         <p className="note">New to trading? Try a Demo account.</p>
         <p className="tc">
-          Terms and Conditions apply<span className="highlight">*</span>. To read the
-          full T&amp;Cs, <a className="highlight underline">click here</a>.
+          Terms and Conditions apply<span className="highlight">*</span>. To
+          read the full T&amp;Cs,{" "}
+          <a className="highlight underline">click here</a>.
         </p>
       </div>
     </div>
